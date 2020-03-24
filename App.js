b857e739f05bc7cafff6094ffc64ed50d3a7237d
Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
2020-03-24 14:09:19 -0300

    Simple play, pause action with expo-av

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useMemo } from 'react';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+export default function App() {
+  const sound = useMemo(() => new Audio.Sound(), []);
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+  useEffect(() => {
+    (async () => {
+      try {
+        await sound.loadAsync({
+          uri:
+            'https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3'
+        });
+      } catch (error) {
+        alert(error);
+      }
+    })();
+  }, []);
+
+  async function play() {
+    try {
+      await sound.playAsync();
+    } catch (error) {
+      alert(error);
+    }
   }
+
+  async function pause() {
+    try {
+      await sound.pauseAsync();
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: 'blue' }]}
+        onPress={play}
+      >
+        <Text style={styles.text}>Play</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: 'red' }]}
+        onPress={pause}
+      >
+        <Text style={styles.text}>Pause</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -23,16 +57,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  button: {
+    width: 100,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  text: {
+    color: '#fff',
+    fontSize: 16
+  }
 });
